2f619ddc472097d5991000ef96acf88041dd4a42
jersey <lizexi711@gmail.com>
2018-05-02 23:06:17 +0800

    webpack环境搭建
    webpack环境搭建

---
+++ src/app.vue
@@ -0,0 +1,21 @@
+<template>
+    <section class="main">
+        <p>我来了{{name}}</p>
+    </section>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            name:"vue + webpack"
+        }
+    }
+}
+</script>
+
+<style>
+.main>p{
+    color: #000;
+}
+</style>
---
+++ src/index.js
@@ -0,0 +1,15 @@
+/*
+* index.js
+* 项目入口文件
+* */
+import Vue from 'vue'
+import App from './app.vue'
+
+// 在body下创建一个根节点
+const root = document.createElement('div');
+document.body.appendChild(root);
+
+// 将根节点root注入到app.vue组件中
+new Vue({
+    render: (h) => h(App)
+}).$mount(root);
