682d2731cc581321b84f2ebfe26e6d4f48387719
jersey <lizexi711@gmail.com>
2018-05-05 21:28:13 +0800

    测试es6写法
    测试es6写法

--- src/app.vue
+++ src/app.vue
@@ -1,14 +1,21 @@
 <template>
     <section class="main">
         <p>我来了{{name}}</p>
+        <button @click = "clkEvt">点击</button>
     </section>
 </template>
 
 <script>
 export default {
     data () {
+    	const uname ='lee';
         return {
-            name:"vue + webpack"
+            name:`vue + webpack${uname}`
+        }
+    },
+    methods: {
+        clkEvt() {
+            console.log(this.name);
         }
     }
 }
